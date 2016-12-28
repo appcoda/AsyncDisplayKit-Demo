2a6f76f4c305accd481a63188a6582111c12cfdd
Simon Ng <simon@Simons-MacBook-Pro-2.local>
2016-12-28 12:01:23 +0800

    Initial Commit

---
+++ BrowseMeetup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrowseMeetup
+//
+//  Created by Simon Ng on 28/12/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
